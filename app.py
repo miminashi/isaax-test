0c74cd29365da0bd03c0c65f9a5ee7eac94707ac
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2016-09-19 20:48:43 +0900

    ADDED - First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib
+
+url='http://127.0.0.1:48080/v1/ping' # isaaxd server ping request
+req=urllib.urlopen(url)
+print("Hello, Isaax! status code:", req.getcode())
+
+while True:
+    print("app is working!")
